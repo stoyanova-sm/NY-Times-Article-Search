f671b73cdb8f9a596d8268eb85b2910038785d34
Svetlana Timina <svetlana_timina@epam.com>
2017-09-17 16:51:18 +0300

    added Item component for single article

---
+++ src/js/components/Item/Item.js
@@ -0,0 +1,28 @@
+// @flow
+import React from 'react';
+import './Item.less';
+
+const Item = (props: {
+  web_url: string,
+  headline: string,
+  pub_date: string,
+  document_type: string
+}) => {
+  let date;
+  let options;
+  if (props.pub_date) {
+    date = new Date(props.pub_date.substring(0, 10));
+    options = { year: 'numeric', month: 'long', day: 'numeric' };
+  }
+
+  return (
+    <div className="article">
+      <a href={props.web_url} target="_blank">{props.headline}</a>
+      {date && <div className="date">Published: {date.toLocaleString('en-US', options)}</div>}
+      <div className="type">Type: {props.document_type}</div>
+    </div>
+  );
+};
+
+export default Item;
+
